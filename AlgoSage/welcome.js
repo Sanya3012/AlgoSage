656790543517ec6c3024fe6ef3bec8ad9ccf7eaf
Sanya3012 <163174669+Sanya3012@users.noreply.github.com>
2024-08-31 10:48:44 +0530

    Add files via upload

---
+++ AlgoSage/welcome.js
@@ -0,0 +1,7 @@
+document.getElementById('farmerBtn').addEventListener('click', function() {
+    window.location.href = 'farmer-registration.html';
+});
+
+document.getElementById('customerBtn').addEventListener('click', function() {
+    window.location.href = 'customer-registration.html';
+});
